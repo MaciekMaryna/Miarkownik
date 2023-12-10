84b4d1c7974d2080f3f517a5da6a8c1a061e2761
Maryna <marynowsk.maciej@gmailcom>
2023-12-10 21:11:05 +0100

    Initial commit 6*ds18b20 + servo. Servo shakes.

---
+++ build/libraries/Servo-1.2.1/avr/Servo.cpp.d
@@ -0,0 +1,15 @@
+C:\Users\maryn\OneDrive\Pulpit\Miarkownik\build\libraries\Servo-1.2.1\avr\Servo.cpp.o: \
+ C:\Users\maryn\OneDrive\Dokumenty\Arduino\libraries\Servo-1.2.1\src\avr\Servo.cpp \
+ C:\Users\maryn\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/Arduino.h \
+ C:\Users\maryn\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/binary.h \
+ C:\Users\maryn\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/WCharacter.h \
+ C:\Users\maryn\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/WString.h \
+ C:\Users\maryn\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/HardwareSerial.h \
+ C:\Users\maryn\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/Stream.h \
+ C:\Users\maryn\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/Print.h \
+ C:\Users\maryn\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/Printable.h \
+ C:\Users\maryn\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/USBAPI.h \
+ C:\Users\maryn\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/Arduino.h \
+ C:\Users\maryn\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\variants\standard/pins_arduino.h \
+ C:\Users\maryn\OneDrive\Dokumenty\Arduino\libraries\Servo-1.2.1\src/Servo.h \
+ C:\Users\maryn\OneDrive\Dokumenty\Arduino\libraries\Servo-1.2.1\src/avr/ServoTimers.h
---
+++ build/sketch/Miarkownik.ino.cpp.d
@@ -0,0 +1,17 @@
+C:\Users\maryn\OneDrive\Pulpit\Miarkownik\build\sketch\Miarkownik.ino.cpp.o: \
+ C:\Users\maryn\OneDrive\Pulpit\Miarkownik\build\sketch\Miarkownik.ino.cpp \
+ C:\Users\maryn\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/Arduino.h \
+ C:\Users\maryn\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/binary.h \
+ C:\Users\maryn\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/WCharacter.h \
+ C:\Users\maryn\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/WString.h \
+ C:\Users\maryn\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/HardwareSerial.h \
+ C:\Users\maryn\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/Stream.h \
+ C:\Users\maryn\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/Print.h \
+ C:\Users\maryn\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/Printable.h \
+ C:\Users\maryn\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/USBAPI.h \
+ C:\Users\maryn\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/Arduino.h \
+ C:\Users\maryn\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\variants\standard/pins_arduino.h \
+ C:\Users\maryn\OneDrive\Dokumenty\Arduino\libraries\Servo-1.2.1\src/Servo.h \
+ C:\Users\maryn\OneDrive\Dokumenty\Arduino\libraries\Servo-1.2.1\src/avr/ServoTimers.h \
+ C:\Users\maryn\OneDrive\Dokumenty\Arduino\libraries\OneWire/OneWire.h \
+ C:\Users\maryn\OneDrive\Dokumenty\Arduino\libraries\DS18B20/DS18B20.h
